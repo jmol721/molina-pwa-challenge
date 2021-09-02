43342226032cc80b3c1767150c3df838bd49e085
jmol721 <80432505+jmol721@users.noreply.github.com>
2021-09-01 22:38:23 -0500

    Fixed some spelling errors on manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,9 +4,10 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
     './index.html',
+    './manifest.json',
     './js/idb.js',
     './js/index.js',
-    './css/style.css',
+    './css/styles.css',
     './icons/icon-72x72.png',
     './icons/icon-96x96.png',
     './icons/icon-128x128.png',
